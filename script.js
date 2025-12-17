bbf607a2f783213d183c5170bbe94848902c434f
mantaroman <emiyakiritsugu44@gmail.com>
2025-12-17 11:29:16 +0700

    Komit Awal

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
